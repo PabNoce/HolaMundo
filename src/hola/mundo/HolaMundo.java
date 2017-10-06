6f33de6431b1559953ddf0b1aadcdb6880f70a33
pnocedalopez <pnocedalopez@victoria15.danielcastelao.org>
2017-10-06 09:33:29 +0200

    Primer commit Comprobacion

--- src/hola/mundo/HolaMundo.java
+++ src/hola/mundo/HolaMundo.java
@@ -6,7 +6,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Cambiando el mundo");
-        System.out.println("Comprobando errores tras subir el .jar");
+        System.out.println("Comprobando otra vez errores tras subir el .jar");
     }
     
 }
